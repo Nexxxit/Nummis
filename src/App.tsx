613229f06f43527badb3267eacf69198628f0bfb
LostProfile <varsik.arsik@mail.ru>
2025-07-30 02:09:21 +0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
 function App() {
   return (
-    <>
-    </>
+    <Routes>
+       <Route element={<Layout />}>
+           <Route index element={<DashboardPage />} />
+           <Route path="/portfolio" element={<PortfolioPage />} />
+           <Route path="/projects" element={<ProjectsPage />} />
+           <Route path="*" element={<NotFoundPage />} />
+       </Route>
+    </Routes>
   )
 }
 
