cfe30137b4f69ac2f0305162d3e1cd3252be57fe
LostProfile <varsik.arsik@mail.ru>
2025-08-02 22:32:24 +0300

    feat: add and connect store

---
+++ src/app/store/store.tsx
@@ -0,0 +1,13 @@
+import {configureStore} from "@reduxjs/toolkit";
+import {portfolioReducer} from "./slices/portfolioSlice.tsx";
+import {projectsReducer} from "./slices/projectsSlice.tsx";
+
+export const store = configureStore({
+    reducer: {
+        portfolio: portfolioReducer,
+        projects: projectsReducer,
+    }
+})
+
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
