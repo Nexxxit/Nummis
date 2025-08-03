4c0af9036bc91d4f6374e35a77b51f55c0fdac5e
LostProfile <varsik.arsik@mail.ru>
2025-08-04 00:58:15 +0300

    style: update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import {Routes, Route} from 'react-router-dom';
+import Layout from "./layouts/Layout.tsx";
+import DashboardPage from "./pages/DashboardPage.tsx";
+import PortfolioPage from "./pages/PortfolioPage.tsx";
+import ProjectsPage from "./pages/ProjectsPage.tsx";
+import NotFoundPage from "./pages/NotFoundPage.tsx";
 
 function App() {
-  return (
-    <Routes>
-       <Route element={<Layout />}>
-           <Route index element={<DashboardPage />} />
-           <Route path="/portfolio" element={<PortfolioPage />} />
-           <Route path="/projects" element={<ProjectsPage />} />
-           <Route path="*" element={<NotFoundPage />} />
-       </Route>
-    </Routes>
-  )
+    return (
+        <Routes>
+            <Route element={<Layout/>}>
+                <Route index element={<DashboardPage/>}/>
+                <Route path="/portfolio" element={<PortfolioPage/>}/>
+                <Route path="/projects" element={<ProjectsPage/>}/>
+                <Route path="*" element={<NotFoundPage/>}/>
+            </Route>
+        </Routes>
+    )
 }
 
 export default App
